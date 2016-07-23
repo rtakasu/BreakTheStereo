775453c2d76e27b75ee41d1b235b50492b36a261
Diego Lazares <dlazares@mit.edu>
2016-07-23 14:38:17 -0700

    Basic template view

--- breakTheStereoApp/admin.py
+++ breakTheStereoApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Person,Song,Reaction
+
+admin.site.register(Person)
+admin.site.register(Song)
+admin.site.register(Reaction)
--- breakTheStereoApp/views.py
+++ breakTheStereoApp/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Song,Person
 # Create your views here.
 def index(request):
-  return HttpResponse("breakTheStereo Y'all")
+  songs = Song.objects.all()
+  #template = loader.get_template('breakTheStereoApp/index.html')
+  #names = ','.join([song.name for song in songs])
+  #if songs:
+  #  return HttpResponse("breakTheStereo Y'all <br>" + names)
+  #else:
+  #  return HttpResponse("breakTheStereo Y'all: <br>" + "No songs found")
+  context = {
+    'songs' : songs,
+  }
+  #return HttpResponse(template.render(context,request))
+  return render(request, 'breakTheStereoApp/index.html',context)
+def profile(request,person_id):
+  try:
+    person = Person.objects.get(pk = person_id)
+  except Person.DoesNotExist:
+    raise Http404("Person does not exist")
+
+    #This should be changed to a template
+  return HttpResponse("Profile info will go here: %s" % person_id)
