7b4080b643e6389e751bf2fd6ba6103e1527c379
rtakasu <rafatakasu@gmail.com>
2016-07-23 17:53:37 -0700

    buttons

--- breakTheStereoApp/static/breakTheStereoApp/app.js
+++ breakTheStereoApp/static/breakTheStereoApp/app.js
@@ -1,4 +1,3 @@
-console.log("Javascript works");
 // using jQuery
 function getCookie(name) {
     var cookieValue = null;
@@ -20,6 +19,7 @@ function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
+
 $.ajaxSetup({
     beforeSend: function(xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
@@ -28,12 +28,83 @@ $.ajaxSetup({
     }
 });
 
-$.ajax({
-    type: "POST",
-    url: "/bts/addReaction/",
-    data: {"person": 1, "song":1, "emotion": "happy"},
-    success: function(data){
-      console.log("response back is:" + data);
-    }
+var widget1
+
+SC.oEmbed('https://soundcloud.com/rafael-takasu/sets/chill', {
+	element: document.getElementById('putTheWidgetHere'),
+	show_comments:false,
+	show_user:false,
+	show_playcount:false,
+	default_height:1000,
+	buying:false,
+	sharing:false,
+	download:false
+}).then(function(embed){
+	var iframeElement   = document.querySelector('iframe');
+	widget1 = SC.Widget(iframeElement);
 });
+
+$( "#happyButton" ).click(function() {
+	widget1.getPosition(function(position){
+			
+		//postEmotion(position,"happy")
+		console.log(position,"happy")
+	})
+});
+
+$( "#chillButton" ).click(function() {
+	widget1.getPosition(function(position){
+			
+		//postEmotion(position,"chill")
+		console.log(position,"chill")
+	})
+});
+
+$( "#peacefulButton" ).click(function() {
+	widget1.getPosition(function(position){
+			
+		//postEmotion(position,"peaceful")
+		console.log(position,"peaceful")
+	})
+});
+
+$( "#runButton" ).click(function() {
+	widget1.getPosition(function(position){
+			
+		//postEmotion(position,"run")
+		console.log(position,"run")
+	})
+});
+
+$( "#singButton" ).click(function() {
+	widget1.getPosition(function(position){
+			
+		//postEmotion(position,"sing")
+		console.log(position,"sing")
+	})
+});
+
+$( "#danceButton" ).click(function() {
+	widget1.getPosition(function(position){
+			
+		//postEmotion(position,"dance")
+		console.log(position,"dance")
+	})
+});
+
+function postEmotion(position, emotion) {
+
+	$.ajax({
+	    type: "POST",
+	    url: "/bts/addReaction/",
+	    data: {"person": 1, "song":1, "emotion": emotion},
+	    success: function(data){
+	      console.log("response back is:" + data);
+	    }
+	});
+
+
+}
+
+
 
