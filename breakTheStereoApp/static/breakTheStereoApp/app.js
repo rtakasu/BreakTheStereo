2717266e13acf768ea1efa536190a151f75ebd48
rtakasu <rafatakasu@gmail.com>
2016-07-23 22:27:54 -0700

    merging

--- breakTheStereoApp/static/breakTheStereoApp/app.js
+++ breakTheStereoApp/static/breakTheStereoApp/app.js
@@ -30,7 +30,7 @@ $.ajaxSetup({
 
 var widget1
 
-SC.oEmbed('https://soundcloud.com/rafael-takasu/sets/s-track-boa-1', {
+SC.oEmbed('https://soundcloud.com/rafael-takasu/sets/breakthestereo', {
 	element: document.getElementById('putTheWidgetHere'),
 	show_comments:false,
 	show_user:false,
@@ -48,31 +48,25 @@ $( "#smileButton" ).click(function() {
 
 	widget1.getCurrentSound(function(song){
 		
-		widget1.getPosition(function(position){
+		console.log("smile", song.id)
+		var counter = parseInt($("#smileCounter").text());
+		$("#smileCounter").text(counter+1)
 
-			console.log(position,"smile", song.id)
-			var counter = parseInt($("#smileCounter").text());
-			$("#smileCounter").text(counter+1)
+		postEmotion(song.id,"smile")
 
-			//postEmotion(position,"smile")
-
-		})
 	})
+
 });
 
 $( "#cryButton" ).click(function() {
 
 	widget1.getCurrentSound(function(song){
 		
-		widget1.getPosition(function(position){
+		console.log("cry", song.id)
+		var counter = parseInt($("#cryCounter").text());
+		$("#cryCounter").text(counter+1)
 
-			console.log(position,"cry", song.id)
-			var counter = parseInt($("#cryCounter").text());
-			$("#cryCounter").text(counter+1)
-
-			//postEmotion(position,"smile")
-
-		})
+		postEmotion(song.id,"cry")
 	})
 });
 
@@ -80,15 +74,11 @@ $( "#aversionButton" ).click(function() {
 	
 	widget1.getCurrentSound(function(song){
 		
-		widget1.getPosition(function(position){
+		console.log("aversion", song.id)
+		var counter = parseInt($("#aversionCounter").text());
+		$("#aversionCounter").text(counter+1)
 
-			console.log(position,"aversion", song.id)
-			var counter = parseInt($("#aversionCounter").text());
-			$("#aversionCounter").text(counter+1)
-
-			//postEmotion(position,"smile")
-
-		})
+		postEmotion(song.id,"aversion")
 	})
 });
 
@@ -96,15 +86,11 @@ $( "#danceButton" ).click(function() {
 	
 	widget1.getCurrentSound(function(song){
 		
-		widget1.getPosition(function(position){
+		console.log("dance", song.id)
+		var counter = parseInt($("#danceCounter").text());
+		$("#danceCounter").text(counter+1)
 
-			console.log(position,"dance", song.id)
-			var counter = parseInt($("#danceCounter").text());
-			$("#danceCounter").text(counter+1)
-
-			//postEmotion(position,"smile")
-
-		})
+		postEmotion(song.id,"dance")
 	})
 });
 
@@ -112,15 +98,11 @@ $( "#chillButton" ).click(function() {
 	
 	widget1.getCurrentSound(function(song){
 		
-		widget1.getPosition(function(position){
+		console.log("chill", song.id)
+		var counter = parseInt($("#chillCounter").text());
+		$("#chillCounter").text(counter+1)
 
-			console.log(position,"chill", song.id)
-			var counter = parseInt($("#chillCounter").text());
-			$("#chillCounter").text(counter+1)
-
-			//postEmotion(position,"smile")
-
-		})
+		postEmotion(song.id,"chill")
 	})
 });
 
@@ -128,15 +110,11 @@ $( "#rockButton" ).click(function() {
 	
 	widget1.getCurrentSound(function(song){
 		
-		widget1.getPosition(function(position){
+		console.log("rock", song.id)
+		var counter = parseInt($("#rockCounter").text());
+		$("#rockCounter").text(counter+1)
 
-			console.log(position,"rock", song.id)
-			var counter = parseInt($("#rockCounter").text());
-			$("#rockCounter").text(counter+1)
-
-			//postEmotion(position,"smile")
-
-		})
+		postEmotion(song.id,"rock")
 	})
 });
 
@@ -144,32 +122,20 @@ $( "#romanticButton" ).click(function() {
 	
 	widget1.getCurrentSound(function(song){
 		
-		widget1.getPosition(function(position){
+		console.log("romantic", song.id)
+		var counter = parseInt($("#romanticCounter").text());
+		$("#romanticCounter").text(counter+1)
 
-			console.log(position,"romantic", song.id)
-			var counter = parseInt($("#romanticCounter").text());
-			$("#romanticCounter").text(counter+1)
-
-			//postEmotion(position,"smile")
-
-		})
+		postEmotion(song.id,"romantic")
 	})
 });
 
-
-// $.ajax({
-//     type: "POST",
-//     url: "/bts/addReaction/",
-//     data: {"person": 1, "song":1, "emotion": "sad"},
-// }).done(function(data){console.log("this is the data"); console.log(data);});
-
-
-function postEmotion(position, emotion) {
+function postEmotion(song, emotion) {
 
 	$.ajax({
 	    type: "POST",
 	    url: "/bts/addReaction/",
-	    data: {"person": 1, "song":1, "emotion": emotion},
+	    data: {"person": 1, "song":song, "emotion": emotion},
 	    success: function(data){
 	      console.log("response back is:" + data);
 	    }
